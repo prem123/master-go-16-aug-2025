705cdd9d3d9ad8cef6dbdd936918c025153f3d8f
Ashish Juyal <ashish.juyal@gmail.com>
2025-09-21 17:20:10 +0530

    ai llm

---
+++ instructor/day-12/llm-rag-go/json.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"mime"
+	"net/http"
+)
+
+// Read JSON request body into a Go struct
+func readRequestJSON(req *http.Request, target any) error {
+	contentType := req.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return err
+	}
+	if mediaType != "application/json" {
+		return fmt.Errorf("expect application/json Content-Type, got %s", mediaType)
+	}
+
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+	return dec.Decode(target)
+}
+
+// Render response as JSON
+func renderJSON(w http.ResponseWriter, v any) {
+	js, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
